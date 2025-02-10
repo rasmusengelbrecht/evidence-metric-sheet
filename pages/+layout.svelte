1c83b59074959fb301a73c4b0d768b1f96ae967e
rasmusengelbrecht <32791593+rasmusengelbrecht@users.noreply.github.com>
2025-02-10 14:58:15 +0000

    Fix logo

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -2,9 +2,10 @@
 	import '@evidence-dev/tailwind/fonts.css';
 	import '../app.css';
 	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	import { base } from '$app/paths'; // Import base path
 	export let data;
 </script>
 
-<EvidenceDefaultLayout {data} logo="/image.webp" hideSidebar=true hideBreadcrumbs=true>
+<EvidenceDefaultLayout {data} logo="{base}/image.webp" hideSidebar=true hideBreadcrumbs=true hideTOC=true>
 	<slot slot="content" />
 </EvidenceDefaultLayout>
